d4334e330d5c206c9e103a347cabc04e939dc527
Evandro Silva Pinto <dev.evandrosilvapinto@gmail.com>
2026-05-25 18:19:34 -0400

    Reapply "Merge pull request #1 from OUD-Tencologia/pagbank"
    This reverts commit b31ffa976ee9d1ad6a18cd7ca910e3a74e56b25a.

---
+++ src/services/pagbank.js
@@ -0,0 +1,32 @@
+import { api } from './api.js'
+
+export const pagBankService = {
+  async getPublicKey() {
+    const res = await api.get('/pagbank/public-key', false)
+    return (res.data ?? res).public_key
+  },
+
+  async createPixOrder(compra_id) {
+    const res = await api.post('/pagbank/orders/pix', { compra_id }, false)
+    return res.data ?? res
+  },
+
+  async createCreditCardOrder(payload) {
+    const res = await api.post(
+      '/pagbank/orders/credit-card',
+      payload,
+      false
+    )
+    return res.data ?? res
+  },
+
+  async getOrderStatus(compra_id) {
+    const res = await api.get(`/pagbank/orders/${compra_id}/status`, false)
+    return res.data ?? res
+  },
+
+  async cancelOrder(compra_id) {
+    const res = await api.post(`/pagbank/orders/${compra_id}/cancel`, {}, false)
+    return res.data ?? res
+  },
+}
